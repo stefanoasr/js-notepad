109588e7a1b89efe73049cc0fe97a820f21b80db
Stefano Marrazzo <stefanom89@gmail.com>
2022-02-14 19:13:15 +0100

    Tabulation using Tab key

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class Main {
 
         this.loadData();
         this.saveData();
+        this.textareaTab();
     }
 
     loadData() {
@@ -21,12 +22,28 @@ class Main {
 
     saveData() {
         var self = this;
-        
+
         var saveIntervalID = setInterval(saveCallback, 5000);
 
         function saveCallback() {
             var text = self.textarea.value;
             localStorage.setItem('data', text);
         }
+    }
+
+    textareaTab() {
+        this.textarea.addEventListener('keydown', function (e) {
+            if (e.key == 'Tab') {
+                e.preventDefault();
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+
+                // set textarea value to: text before caret + tab + text after caret
+                this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+                // put caret at right position again
+                this.selectionStart = this.selectionEnd = start + 1;
+            }
+        });
     }
 }
